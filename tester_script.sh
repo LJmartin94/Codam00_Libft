12afc009720a4a3bb05da83ccd7037ab79888dc0
LJmartin94 <lindsayjamesmartin94@gmail.com>
2022-06-08 11:25:41 +0200

    Script now also checks norminette plus

--- tester_script.sh
+++ tester_script.sh
@@ -1,8 +1,15 @@
+#!/bin/sh
 echo "\033[0;32m Directory structure: This dir should contain the script, a dir with libft (Libft) and a dir with the tests (Tests) \033[0m"
 rm -rf output
 touch output
 echo "Checking norminette"
+echo "Norminette: " >> output
 cd Libft && norminette | cat -e | grep "Error" >> ../output
+cd ..
+echo "Checking norminette+"
+echo "Norminette+: " >> output
+cd Libft 
+curl -sL bit.ly/qnorm | bash | grep -B 1 "Error" >> ../output
 cd ..
 rm -rf compilation_info
 touch compilation_info
@@ -10,3 +17,4 @@ echo "Compiling"
 cd Libft && make | cat -e >> ../compilation_info && cd ..
 echo "Cleaning up"
 cd Libft && make fclean | cat -e >> ../compilation_info && cd ..
+open output
