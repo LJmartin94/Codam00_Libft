cf21d31551d918834333943fc8ee855a2066d782
LJmartin94 <lindsayjamesmartin94@gmail.com>
2022-06-08 10:15:51 +0200

    Making a tester script to run all tests on a given libft.

---
+++ tester_script.sh
@@ -0,0 +1,11 @@
+echo "\033[0;32m Directory structure: This dir should contain the script, a dir with libft (Libft) and a dir with the tests (Tests) \033[0m"
+rm -rf output
+touch output
+echo "Checking norminette"
+cd Libft && norminette | cat -e >> ../output && cd ..
+rm -rf compilation_info
+touch compilation_info
+echo "Compiling"
+cd Libft && make | cat -e >> ../compilation_info && cd ..
+echo "Cleaning up"
+cd Libft && make fclean | cat -e >> ../compilation_info && cd ..
